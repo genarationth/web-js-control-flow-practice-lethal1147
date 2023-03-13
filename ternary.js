3958229ba98239dd2ba41d7f0f0e4e4fc3093945
Joakim <55298128+lethal1147@users.noreply.github.com>
2023-03-13 14:37:23 +0700

    Add ternary.js

---
+++ ternary.js
@@ -0,0 +1,4 @@
+// Ternary Operator
+let password = 'thisPasswordSoStrong';
+password.length >= 12 ? console.log('password is very strong! so WELCOME!!') : password.lenght >= 8 ? console.log('WELCOME!!!') : console.log('SORRY!!! the password is too short!');
+
