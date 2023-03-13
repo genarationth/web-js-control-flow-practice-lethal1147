0696f9b76f84a282251815e0a444ea1505ca997e
Joakim <55298128+lethal1147@users.noreply.github.com>
2023-03-13 14:51:29 +0700

    Add files via upload

--- ternary.js
+++ ternary.js
@@ -1,4 +1,6 @@
 // Ternary Operator
-let password = 'thisPasswordSoStrong';
-password.length >= 12 ? console.log('password is very strong! so WELCOME!!') : password.lenght >= 8 ? console.log('WELCOME!!!') : console.log('SORRY!!! the password is too short!');
+let password = '11111111';
+password.length >= 12 ? console.log('password is very strong!')
+:password.length >= 8 ? console.log('WELCOME!!!') 
+:console.log('SORRY!!! the password is too short!');
 
